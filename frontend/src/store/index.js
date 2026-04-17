34e675d88c75760a28392cad45ff8e1826c74986
Michael Jonah Akyene <michael.akyene@stu.ucc.edu.gh>
2026-04-17 08:46:59 +0000

    Delete frontend directory

--- frontend/src/store/index.js
+++
@@ -1,5 +0,0 @@
-import useAuthStore from './useAuthStore';
-import useQuizStore from './useQuizStore';
-import useNotificationStore from './useNotificationStore';
-
-export { useAuthStore, useQuizStore, useNotificationStore };
